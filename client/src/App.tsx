ad98a232e164333507af4a4bc97c0c2c6747bd5b
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-07-29 01:22:50 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { TooltipProvider } from '@/components/ui/tooltip';
 import LandingPage from './features/landing/LandingPage';
 import ApplicationWizard from './features/application/ApplicationWizard';
 import LoginPage from './features/auth/LoginPage';
+import AdminLayout from './features/admin/layouts/AdminLayout';
+import DashboardPage from './features/admin/pages/DashboardPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/apply" element={<ApplicationWizard />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <TooltipProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/apply" element={<ApplicationWizard />} />
+          <Route path="/login" element={<LoginPage />} />
+          
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<DashboardPage />} />
+            {/* Add more nested admin routes here later */}
+          </Route>
+        </Routes>
+      </Router>
+    </TooltipProvider>
   );
 }
 
