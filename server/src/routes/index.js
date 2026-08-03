874b4b405ef84c75acbc6ee37a8869ea555c3308
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-08-04 00:14:10 +0800

    Delete index.js

--- server/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { checkHealth } = require('../controllers/healthController');
-
-// Health check route
-router.get('/health', checkHealth);
-
-module.exports = router;
