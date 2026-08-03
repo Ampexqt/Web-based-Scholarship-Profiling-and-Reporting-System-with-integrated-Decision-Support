9e4ee5cdd9d0bdd40ced9e65640c5bb40ac6f0e2
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-08-03 22:26:07 +0800

    Create academicStructure.ts

---
+++ client/src/features/application/data/academicStructure.ts
@@ -0,0 +1,70 @@
+export const academicStructure = [
+  {
+    name: 'College of Teacher Education',
+    abbr: 'CTE',
+    programs: [
+      { name: 'Bachelor of Elementary Education', abbr: 'BEE', majors: [] },
+      { name: 'Bachelor of Secondary Education', abbr: 'BSE', majors: ['Major in Mathematics', 'Major in English'] },
+      { name: 'Bachelor of Technical-Vocational Teacher Education', abbr: 'BTTE', majors: ['Major in Heating, Ventilation, and Air-Conditioning Technology', 'Major in Automotive Technology', 'Major in Civil Technology', 'Major in Drafting Technology', 'Major in Electrical Technology', 'Major in Electronics Technology', 'Major in Food and Services Management', 'Major in Garments, Fashion, and Design', 'Major in Mechanical Technology', 'Major in Welding and Fabrication Technology'] },
+      { name: 'Bachelor of Technology and Livelihood Education', abbr: 'BTLE', majors: ['Major in Home Economics', 'Major in Industrial Arts', 'Major in Information Communication Technology'] }
+    ]
+  },
+  {
+    name: 'College of Physical Education and Sports',
+    abbr: 'CPES',
+    programs: [
+      { name: 'Bachelor of Physical Education (BPED)', abbr: 'BPE', majors: [] },
+      { name: 'Bachelor of Science in Exercise and Sports Sciences', abbr: 'BSESS', majors: ['Major in Fitness and Sports Coaching', 'Major in Fitness and Sports Management'] }
+    ]
+  },
+  {
+    name: 'College of Maritime Education',
+    abbr: 'CME',
+    programs: [
+      { name: 'Bachelor of Science in Marine Engineering', abbr: 'BSME', majors: [] }
+    ]
+  },
+  {
+    name: 'College of Information and Computing Sciences',
+    abbr: 'CICS',
+    programs: [
+      { name: 'Bachelor of Science in Information Technology', abbr: 'BSIT', majors: [] },
+      { name: 'Bachelor of Science in Information Systems', abbr: 'BSIS', majors: [] }
+    ]
+  },
+  {
+    name: 'College of Engineering and Technology',
+    abbr: 'CET',
+    programs: [
+      { name: 'Bachelor of Science in Civil Engineering', abbr: 'BSCE', majors: [] },
+      { name: 'Bachelor of Industrial Technology', abbr: 'BIT', majors: ['Major in Apparel & Fashion Technology', 'Major in Architectural Drafting Technology', 'Major in Automotive Technology', 'Major in Civil Technology', 'Major in Computer Technology', 'Major in Culinary Technology', 'Major in Electrical Technology', 'Major in Electronics Technology', 'Major in Heating, Ventilation, Air Conditioning, & Refrigeration Technology', 'Major in Mechanical Technology', 'Major in Mechatronics Technology', 'Major in Power Plant Technology'] }
+    ]
+  },
+  {
+    name: 'College of Arts, Humanities, and Social Sciences',
+    abbr: 'CAHSS',
+    programs: [
+      { name: 'Bachelor of Science in Development Communication', abbr: 'BSDC', majors: [] },
+      { name: 'Batsilyer ng Sining sa Filipino', abbr: 'BSF', majors: [] },
+      { name: 'Bachelor in Fine Arts', abbr: 'BFA', majors: ['Major in Industrial Design'] }
+    ]
+  },
+  {
+    name: 'School of Business Administration',
+    abbr: 'SBA',
+    programs: [
+      { name: 'Bachelor of Science in Entrepreneurship', abbr: 'BSE', majors: [] },
+      { name: 'Bachelor of Science in Hospitality Management', abbr: 'BSHM', majors: [] }
+    ]
+  },
+  {
+    name: 'Pre-Baccalaureate / Non-Degree Programs',
+    abbr: 'PNP',
+    programs: [
+      { name: '2-Year Associate in Industrial Technology', abbr: 'AIT', majors: ['Major in Automotive Technology', 'Major in Electronics Technology', 'Major in Electrical Technology', 'Major in Food Technology', 'Major in Garments & Textiles Technology', 'Major in Refrigeration & Air-Conditioning Technology'] },
+      { name: '2-Year Trade Technical Education', abbr: 'TTE', majors: ['Major in Architectural Drafting Technology'] },
+      { name: '3-Year Diploma of Technology', abbr: 'DT', majors: ['Major in Automotive Engineering Technology', 'Major in Civil Engineering Technology', 'Major in Electrical Engineering Technology', 'Major in Hospitality Management Technology', 'Major in Electronics and Communication Technology', 'Major in Food Production & Services Management Technology', 'Major in Garments, Fashion & Design Technology', 'Major in Information Technology', 'Major in Mechanical Engineering Technology'] },
+      { name: '3-Year Trade Industrial Technical Education', abbr: 'TITE', majors: ['Major in Welding & Fabrication Technology'] }
+    ]
+  }
+];
